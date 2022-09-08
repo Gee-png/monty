384300e3ecb9b2dfbbdbdebe04bb4dba0eeb75b8
Gee-png <gloryamuah@gmail.com>
2022-09-08 14:48:25 -0700

    Changes

--- monty.h
+++ monty.h
@@ -47,8 +47,6 @@ typedef struct stack_s
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- * Authors: Martins Akhivbareme & Bolatito Alaba
- *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
  */
